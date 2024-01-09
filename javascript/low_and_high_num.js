d50ac7108634f654f3a3378683a6233a8790439d
Neuro Encarnacion <neuro_535@hotmail.com>
2024-01-09 11:57:19 -0500

    finishing up new algo

--- javascript/low_and_high_num.js
+++ javascript/low_and_high_num.js
@@ -1,10 +1,22 @@
 // Find the lowest and highest numbers and insert them into an array
 
 function minMax(arr){
-    var lowHigh = []
-    Math.min(arr).push(lowHigh)
-    Math.max(arr).push(lowHigh)
-    return lowHigh
+    var lowest = arr[0];
+    var highest = arr[0];
+    var emptyArr = [];
+    for (var i=1; i < arr.length; i++) {
+        if (arr[i] < lowest) {
+            lowest = arr[i];
+        }
+    }
+    for (var i=1; i < arr.length; i++) {
+        if (arr[i] > highest) {
+            highest = arr[i];
+        }
+    }
+    emptyArr.push(lowest)
+    emptyArr.push(highest)
+    return emptyArr
 }
 
-console.log(minMax([0, 5, 7, 1, 3, 9, 15, 19]))
+console.log(minMax([10, 5, 7, 1, 3, 9, 15, 19]))
