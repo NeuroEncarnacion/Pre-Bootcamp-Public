521e5eedd6390b774640b7d6a5ab3ba464f6e88e
Neuro Encarnacion <neuro_535@hotmail.com>
2024-01-14 21:59:04 -0500

    new python algos added

---
+++ python/difference_max_min.py
@@ -0,0 +1,13 @@
+# Create a function that takes a list and returns the difference between the biggest and smallest numbers.
+
+def difference_max_min(lst):
+	max = lst[0]
+	min = lst[0]
+	for num in lst:
+		if max < num:
+			max = num
+		if min > num:
+			min = num
+	return max - min
+
+print(difference_max_min([10,4,1,2,8,91]))
---
+++ python/fps.py
@@ -0,0 +1,7 @@
+# Create a function that returns the number of frames shown in a given number of minutes for a certain FPS.
+
+def frames(minutes, fps):
+	total_seconds = minutes * 60
+	return total_seconds * fps
+
+print(frames(52, 33))
