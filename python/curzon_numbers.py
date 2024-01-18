e81082d64864d935d053b95d102f2f638b36fef5
Neuro Encarnacion <neuro_535@hotmail.com>
2024-01-17 21:00:22 -0500

    new curzon_numbers algo added to python

---
+++ python/curzon_numbers.py
@@ -0,0 +1,13 @@
+# Establish if a given integer num is a Curzon number. If 1 plus 2 elevated to num is exactly divisible by 1 plus 2 multiplied by num, then num is a Curzon number.
+# Given a non-negative integer num, implement a function that returns True if num is a Curzon number, or False otherwise.
+
+
+def is_curzon(num):
+	a = 2 ** num + 1 
+	b = 2 * num + 1 
+	if (a % b == 0):
+		return True
+	else:
+		return False
+
+print(is_curzon(293))
