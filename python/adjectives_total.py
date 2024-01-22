374e10a730dc74e522a567b0121601516d2459e4
Neuro Encarnacion <neuro_535@hotmail.com>
2024-01-22 12:35:20 -0500

    new python algos completed

---
+++ python/adjectives_total.py
@@ -0,0 +1,18 @@
+# You call your spouse in anger and a "little" argument takes place. Count the total amount of insults used. Given a dictionary of insults, return the total amount of insults used.
+# The dictionary will never be empty
+import random
+
+def total_amount_adjectives(dct):
+	total_insults = 0
+	for item in dct:
+		total_insults	+= 1
+	return total_insults
+
+obj2 = { 'a': "moron", 'b': "scumbag", 'c': "moron", 'd': "dirtbag" }
+
+# rand=random.randint(10,20)
+# dynamic={i:j for i,j in list(map(lambda x:(chr(65+x),x),list(range(rand))))}
+
+
+print(total_amount_adjectives(obj2))
+# print(total_amount_adjectives(dynamic))
---
+++ python/sum_of_resistance.py
@@ -0,0 +1,14 @@
+# When resistors are connected together in series, the same current passes through each resistor in the chain and the total resistance, RT, of the circuit must be equal to the sum of all the individual resistors added together. That is: RT = R1 + R2 + R3
+
+# Create a function that takes an array of values resistance that are connected in series, and calculates the total resistance of the circuit in ohms. The ohm is the standard unit of electrical resistance in the International System of Units ( SI ).
+
+def series_resistance(lst):
+	total_resistance = 0
+	for i in lst:
+		total_resistance += i
+	if total_resistance <= 1:
+		return(str(total_resistance) + " ohm")
+	else:
+		return(str(total_resistance) + " ohms")
+
+print(series_resistance([10, 12, 32, 4.9, 5, 6, 71]))
